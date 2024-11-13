295ec20abe329ede19c6ff56237b3fd2981d7265
Squeezr <samxiezx1115@gmail.com>
2024-11-13 09:35:21 -0500

    main:res address model in place and relation defined

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from database import engine, SessionLocal  # from the ./database.py
 from datetime import datetime
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
 
@@ -47,31 +46,45 @@ async def get_applications(db: Session = Depends(get_db)):
     """
     Get ALL applications
     """
-    results = db.query(schemas.Applications).all()
+    results = db.query(schemas.Application).all()
     return results
 
 
 @app.post("/create")
 async def create_application(application: App, db: Session = Depends(get_db)):
     """
-    Create ONE application
+    Create ONE application, NOT SAVE
     """
 
     application_status = application.app_status
     timestamp_in_seconds = int(datetime.now().timestamp())
+    app_id = str(uuid4())
 
-    application_model = schemas.Applications()
-    application_model.app_id = str(uuid4())
+    application_model = schemas.Application()
+    application_model.app_id = app_id
     application_model.app_status = application_status
     application_model.change_date = timestamp_in_seconds
+
+    res_address_model = schemas.ResidentialAddress()
+    res_address_model.app_id = app_id
+    res_address_model.res_id = app_id
+
     db.add(application_model)
+    db.add(res_address_model)
     db.commit()
     db.refresh(application_model)
 
-    return JSONResponse(
-        status_code=200,
-        content={"msg": "SUCCESS, application created"},
-    )
+    return application_model
+
+
+# @app.post("/save")
+# async def save_application(application: App, db: Session = Depends(get_db)):
+#     """
+#     save an application
+#     """
+#     res_address_model = schemas.ResidentialAddress()
+#     res_address_model.res_id = str(uuid4())
+#     return
 
 
 @app.delete("/delete")
@@ -80,7 +93,7 @@ async def delete_application(appDelete: AppDelete, db: Session = Depends(get_db)
     Delete ONE application
     """
     app_id = appDelete.app_id
-    application = db.query(schemas.Applications).filter(schemas.Applications.app_id == app_id).first()
+    application = db.query(schemas.Application).filter(schemas.Application.app_id == app_id).first()
     if not application:
         raise HTTPException(status_code=404, detail="Application not found")
 
--- schemas.py
+++ schemas.py
@@ -1,15 +1,32 @@
 from database import Base
-from sqlalchemy import Column, String, Integer, DATE
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Applications(Base):
+class Application(Base):
     """
     appID VARCHAR
     appStatus INT -- 1, 2, 3, 4, 5-finished
     changeDate DATE
     """
 
-    __tablename__ = "applications"
+    __tablename__ = "application_table"
     app_id = Column(String, primary_key=True, index=True)
     app_status = Column(Integer)
     change_date = Column(Integer)
+
+    application = relationship("ResidentialAddress", back_populates="r_address")
+
+
+class ResidentialAddress(Base):
+    __tablename__ = "address_table"
+    res_id = Column(String, primary_key=True, index=True)
+    app_id = Column(String, ForeignKey("application_table.app_id"))
+    province = Column(String)  # drop down
+    postalcode = Column(String)
+    city = Column(String)
+    province = Column(String)
+    street = Column(String)
+    street_num = Column(Integer)
+
+    r_address = relationship("Application", back_populates="application")
